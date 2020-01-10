b4bfc16e987d2701bf42ccb711533477a98cc6b2
Luzaks <kanemekanik@gmail.com>
2020-01-10 15:25:13 -0600

    Make the form_for tag

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,11 @@
 class UsersController < ApplicationController
     def new
+        @user = User.new
     end
 
     def create
-        @user = User.new(username: params[:username], email: params[:email], password: params[:password])        # @user = User.new(user_params)
+        #@user = User.new(username: params[:username], email: params[:email], password: params[:password])        # 
+        @user = User.new(user_params)
         
         if @user.save
           redirect_to new_user_path
@@ -14,9 +16,9 @@ class UsersController < ApplicationController
     end
 end
 
-    # private
+    private
 
-    # def user_params
-    #   params.require(:user).permit(:username, :email, :password,
-    #                                :password_confirmation)
-    # end
+    def user_params
+      params.require(:user).permit(:username, :email, :password,
+                                   :password_confirmation)
+    end
